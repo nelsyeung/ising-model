8d405672cd78fabcd6b16ced66ab21b3dd24a49e
Nelson Yeung <nelsyeung@icloud.com>
2015-06-10 21:35:22 +0100

    Add worker thread and major improvements

---
+++ main.js
@@ -0,0 +1,114 @@
+var numElectronsInput = document.getElementById('num-electrons-input');
+var startBtn = document.getElementById('start-btn');
+var stopBtn = document.getElementById('stop-btn');
+var totalSpinEl = document.getElementById('total-spin');
+var polarisationEl = document.getElementById('polarisation');
+var spins;
+var simWorker;
+
+function changeNumElectrons() {
+	document.getElementById('num-electrons-label').innerHTML = numElectronsInput.value;
+}
+
+function initSpins() {
+	var numElectrons = numElectronsInput.value;
+	var totalSpin = 0;
+	spins = new Array(numElectrons);
+
+	for (var i = 0; i < numElectrons; ++i) {
+		spins[i] = new Array(numElectrons);
+
+		for (var j = 0; j < numElectrons; ++j) {
+			if (Math.round(Math.random()) === 0) {
+				spins[i][j] = -1;
+				totalSpin -= 1;
+			} else {
+				spins[i][j] = 1;
+				totalSpin += 1;
+			}
+		}
+	}
+
+	totalSpinEl.innerHTML = Math.abs(totalSpin);
+	polarisationEl.innerHTML = (Math.abs(totalSpin) / (spins.length * spins.length)).toFixed(5);
+
+	stopSim();
+}
+
+function drawCanvas() {
+	var canvas = document.getElementById('canvas');
+	var numElectrons = numElectronsInput.value;
+	var padding = 3;
+	var size = (canvas.offsetWidth - 10 - padding * (numElectrons - 1)) / numElectrons;
+
+	if (canvas.getContext) {
+		var ctx = canvas.getContext('2d');
+
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+		for (var i = 0; i < spins.length; ++i) {
+			for (var j = 0; j < spins.length; ++j) {
+				if (spins[i][j] === -1) {
+					ctx.fillStyle = '#000';
+				} else {
+					ctx.fillStyle = '#DDD';
+				}
+
+				ctx.fillRect(i * (size + padding), j * (size + padding), size, size);
+			}
+		}
+	}
+}
+
+function simulate() {
+	if (window.Worker) {
+		if (typeof(simWorker) !== 'undefined') {
+			simWorker.postMessage([
+					document.getElementById('J').value,
+					document.getElementById('B').value,
+					document.getElementById('T').value,
+					spins
+			]);
+
+			simWorker.onmessage = function(e) {
+				spins = e.data[0];
+				drawCanvas();
+				totalSpinEl.innerHTML = e.data[1];
+				polarisationEl.innerHTML = e.data[2];
+				setTimeout(simulate, document.getElementById('time-interval').value * 1000);
+			};
+		}
+	}
+	else {
+		document.getElementsByTagName('body')[0].innerHTML = 'This simulation requires a newer browser. Your browser does not support Web Workers.';
+	}
+}
+
+function startSim() {
+	startBtn.disabled = true;
+	stopBtn.disabled = false;
+
+	if (typeof(simWorker) === 'undefined') {
+		simWorker = new Worker('worker.js');
+	}
+
+	simulate();
+}
+
+function stopSim() {
+	startBtn.disabled = false;
+	stopBtn.disabled = true;
+
+	if (typeof(simWorker) !== 'undefined') {
+		simWorker.terminate();
+		simWorker = undefined;
+	}
+}
+
+window.onload = function() {
+	initSpins();
+	drawCanvas();
+	changeNumElectrons();
+	startBtn.disabled = false;
+	stopBtn.disabled = true;
+};
---
+++ worker.js
@@ -0,0 +1,38 @@
+function calcSpins(J, B, T, prevSpins) {
+	var totalSpin = 0;
+	var spins = prevSpins;
+	var top, right, bottom, left;
+	var M, B_eff, tmp, P_up;
+
+	for (var i = 0; i < spins.length; ++i) {
+		for (var j = 0; j < spins.length; ++j) {
+			top = (j - 1 < 0) ? (spins.length - 1) : (j - 1);
+			right = (i + 1 >= spins.length) ? 0 : (i + 1);
+			bottom = (j + 1 >= spins.length) ? 0 : (j + 1);
+			left = (i - 1 < 0) ? (spins.length - 1) : (i - 1);
+
+			M = prevSpins[i][top] + prevSpins[right][j] + prevSpins[i][bottom] + prevSpins[left][j];
+			B_eff = J * M + B;
+			tmp = Math.exp(B_eff / T);
+			P_up = tmp / (tmp + Math.pow(tmp, -1));
+
+			if (Math.floor(Math.random() + P_up) >= 1) {
+				spins[i][j] = 1;
+			} else {
+				spins[i][j] = -1;
+			}
+
+			totalSpin += spins[i][j];
+		}
+	}
+
+	postMessage([
+		spins,
+		Math.abs(totalSpin), // M
+		(Math.abs(totalSpin) / (spins.length * spins.length)).toFixed(5) // P
+	]);
+}
+
+onmessage = function(e) {
+	calcSpins(e.data[0], e.data[1], e.data[2], e.data[3]);
+};
